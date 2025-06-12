3eaca42dc0cec78aaa3113454d3d322cc8782011
CalmXin <skyqian@88.com>
2025-06-12 23:52:31 +0800

    feat: 添加 Gunicorn 配置文件和启动脚本
    - 新增 gunicorn_config.py 文件，配置 Gunicorn 服务器参数
    - 新增 run.sh 脚本，用于启动虚拟环境和 Gunicorn服务
    - 配置项目目录、进程数、线程数、日志路径等
    - 实现虚拟环境检测和自动创建功能

---
+++ gunicorn_config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+_BASE_DIR = Path(__file__).parent
+
+# 项目目录
+chdir = str(_BASE_DIR.resolve())
+
+# 指定进程数
+workers = 2
+
+# 指定每个进程开启的线程数
+threads = 16
+
+# 启动模式
+worker_class = 'sync'
+
+# 绑定的ip与端口
+bind = '0.0.0.0:12345'
+
+# 设置访问日志和错误信息日志路径
+accesslog = f'{chdir}/log/gunicorn_access.log'
+errorlog = f'{chdir}/log/gunicorn_error.log'
+
+loglevel = 'info'
---
+++ run.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+WORK_DIR=$(dirname "$0")
+VENV_DIR="$WORK_DIR/venv"
+CONFIG_FILE="gunicorn_config.py"
+cd "$WORK_DIR" || exit
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "虚拟环境不存在，正在创建……"
+    python3 -m venv "$VENV_DIR"
+
+    source "$VENV_DIR/bin/activate"
+    pip install -r "$WORK_DIR/requirements.txt"
+    pip install gunicorn
+else
+  echo "检测到虚拟环境，开始执行……"
+    source "$VENV_DIR/bin/activate"
+fi
+
+exec gunicorn -c $CONFIG_FILE app:app
